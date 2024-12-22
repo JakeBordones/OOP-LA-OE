877d697d53326bf5fd7daae341c9600f625504e1
JakeBordones <jakebordones237@gmail.com>
2024-12-22 23:25:19 +0800

    Add files via upload

---
+++ OE/OE#7.py
@@ -0,0 +1,16 @@
+
+class TekkenCharacter:
+    def __init__(self, name, ability): 
+        self.name = name
+        self.ability = ability
+    def introduce(self, function):
+        def wrapper():
+            print("Introducing.....")
+            function()
+            print("This Character is amazing!!!!")
+        return wrapper
+charac = TekkenCharacter("Asuka", "Combo Damage")
+@charac.introduce
+def character_intro():
+    print(f"I am {charac.name} and my ability is {charac.ability}")
+character_intro()
